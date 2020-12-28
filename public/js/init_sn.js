0daac18f034f79036ed45fe201755261d3e0c834
noevenmore <60346860+noevenmore@users.noreply.github.com>
2020-12-28 09:17:11 +0200

    summernote loadimages bugfix

--- public/js/init_sn.js
+++ public/js/init_sn.js
@@ -4,6 +4,73 @@ $.ajaxSetup({
   }
 });
 
+function uploadImage(image) {
+  var data = new FormData();
+  data.append("type","etc");
+  data.append("image[0]", image);
+  $.ajax({
+      url: '/admin/loadimage',
+      cache: false,
+      contentType: false,
+      processData: false,
+      data: data,
+      mimeType:'multipart/form-data',
+      type: "post",
+      success: function(url) {
+          let res = JSON.parse(url)
+
+          var image = $('<img>').attr('src', '/upload/images/' + res.image[0]);
+          $('#summernote').summernote("insertNode", image[0]);
+      },
+      error: function(data) {
+          console.log(data);
+      }
+  });
+}
+
+function uploadImage_eng(image) {
+var data = new FormData();
+data.append("type","etc");
+data.append("image[0]", image);
+$.ajax({
+    url: '/admin/loadimage',
+    cache: false,
+    contentType: false,
+    processData: false,
+    data: data,
+    mimeType:'multipart/form-data',
+    type: "post",
+    success: function(url) {
+        let res = JSON.parse(url)
+
+        var image = $('<img>').attr('src', '/upload/images/' + res.image[0]);
+        $('#summernote_eng').summernote("insertNode", image[0]);
+    },
+    error: function(data) {
+        console.log(data);
+    }
+});
+}
+
+function deleteFile(src) {
+let filename = src.split('/').pop();
+
+$.ajax({
+    data: {'filename':filename},
+    type: "POST",
+    url: '/admin/deleteimage',
+    cache: false,
+    error: function(resp) {
+        console.log(resp);
+    }
+});
+}
+
+flatpickr("#datatimepicker", {
+enableTime: true,
+dateFormat: "Y-m-d H:i",
+});
+
 let summernote_param={
   placeholder: 'Введите текст...',
   toolbar: [
@@ -29,48 +96,31 @@ let summernote_param={
 }
 };
 
+
+let summernote_param_eng={
+  placeholder: 'Введите текст...',
+  toolbar: [
+  ['clear',['clear']],
+    ['style', ['style']],
+    ['font', ['bold', 'italic', 'underline','fontsize']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['table', ['table']],
+    ['insert', ['link']],
+    ['view', ['fullscreen', 'codeview']],
+    ['ex', ['link','picture','video']],
+    ['control',['undo','redo']]
+  ],
+  callbacks: {
+    onImageUpload: function(image) {
+        uploadImage_eng(image[0]);
+    },
+
+    onMediaDelete : function(target) {
+      deleteFile(target[0].src);
+  }
+}
+};
+
 $('#summernote').summernote(summernote_param);
-$('#summernote_eng').summernote(summernote_param);
-
-function uploadImage(image) {
-    var data = new FormData();
-    data.append("type","etc");
-    data.append("image[0]", image);
-    $.ajax({
-        url: '/admin/loadimage',
-        cache: false,
-        contentType: false,
-        processData: false,
-        data: data,
-        mimeType:'multipart/form-data',
-        type: "post",
-        success: function(url) {
-            let res = JSON.parse(url)
-
-            var image = $('<img>').attr('src', '/upload/images/' + res.image[0]);
-            $('#summernote').summernote("insertNode", image[0]);
-        },
-        error: function(data) {
-            console.log(data);
-        }
-    });
-}
-
-function deleteFile(src) {
-  let filename = src.split('/').pop();
-
-  $.ajax({
-      data: {'filename':filename},
-      type: "POST",
-      url: '/admin/deleteimage',
-      cache: false,
-      error: function(resp) {
-          console.log(resp);
-      }
-  });
-}
-
-flatpickr("#datatimepicker", {
-  enableTime: true,
-  dateFormat: "Y-m-d H:i",
-});
+$('#summernote_eng').summernote(summernote_param_eng);
